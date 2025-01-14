9c74ce6e4a56d2b811aac1fb1d4b2cd6f3e8896c
Eduardo Bonifacio <116756404+edu-boni@users.noreply.github.com>
2025-01-14 14:27:49 -0300

    Create Menu.java

---
+++ Projeto_INOO/src/com/clinica_de_animais/projeto/model/Menu.java
@@ -0,0 +1,83 @@
+package com.clinica_de_animais.projeto.model;
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner leitura;
+
+    public Menu(Scanner leitura) {
+        this.leitura = leitura;
+    }
+
+    public void exibirMenuFuncionario() {
+        System.out.println("\nSelecione qual funcionário você é:\n" +
+                           "1. Administrador\n" +
+                           "2. Groomer\n" +
+                           "3. Veterinário\n" +
+                           "0. Voltar");
+        System.out.print("Escolha uma opção: ");
+
+        if (!leitura.hasNextInt()) {
+            System.out.println("Por favor, insira um número válido.");
+            leitura.nextLine();
+            return;
+        }
+
+        int opcao = leitura.nextInt();
+        leitura.nextLine();
+
+        switch (opcao) {
+            case 1:
+                exibirMenuAdministrador(); 
+                break;
+            case 2:
+                //gromer
+                break;
+            case 3:
+                //vet
+                break;
+            case 0:
+                System.out.println("Voltando ao menu principal.");
+                break;
+            default:
+                System.out.println("Opção inválida. Tente novamente.");
+        }
+    }
+
+    public void exibirMenuAdministrador() {
+        while (true) {
+            System.out.println("\nBem-vindo Administrador!");
+            System.out.println("1. Cadastrar novo funcionário\n" +
+                               "2. Cadastrar animal para adoção\n" +
+                               "3. Listar todos funcionários, usuários e animais\n" +
+                               "0. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            if (!leitura.hasNextInt()) {
+                System.out.println("Por favor, insira um número válido.");
+                leitura.nextLine();
+                continue;
+            }
+
+            int opcao_adm = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (opcao_adm) {
+                case 1:
+                //cadastarr funcinoario
+                    break;
+                case 2:
+                //cadastarar nimal
+                    break;
+                case 3:
+                    //listar tuido
+                    Dono.listarDonos();
+                    break;
+                case 0:
+                    System.out.println("Voltando ao menu de funcionários.");
+                    return; 
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+}
