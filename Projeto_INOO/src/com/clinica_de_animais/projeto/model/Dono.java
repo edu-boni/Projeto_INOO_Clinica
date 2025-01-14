bc3f00d628c608e2c95edeffaf7747ce9ee6f222
Eduardo Bonifacio <116756404+edu-boni@users.noreply.github.com>
2025-01-14 14:01:23 -0300

    Update Dono.java

--- Projeto_INOO/src/com/clinica_de_animais/projeto/model/Dono.java
+++ Projeto_INOO/src/com/clinica_de_animais/projeto/model/Dono.java
@@ -1,4 +1,5 @@
 package com.clinica_de_animais.projeto.model;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -20,50 +21,44 @@ public class Dono {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getTelefone() {
         return telefone;
     }
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
     public String getEndereco() {
         return endereco;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public void cadastrarDono() {
-        Scanner ler = new Scanner(System.in);
-
+    public static void cadastrarDono(Scanner leitura) {
         System.out.print("Digite o nome do dono: ");
-        String nome = ler.nextLine();
+        String nome = leitura.nextLine();
         System.out.print("Digite o email do dono: ");
-        String email = ler.nextLine();
+        String email = leitura.nextLine();
         System.out.print("Digite o telefone do dono: ");
-        String telefone = ler.nextLine();
+        String telefone = leitura.nextLine();
         System.out.print("Digite o endereço do dono: ");
-        String endereco = ler.nextLine();
+        String endereco = leitura.nextLine();
 
         Dono novoDono = new Dono(nome, email, telefone, endereco);
         donos.add(novoDono);
         System.out.println("Dono cadastrado com sucesso!");
+    }
 
-        ler.close(); 
+    public static void listarDonos() {
+        if (donos.isEmpty()) {
+            System.out.println("Não há donos cadastrados.");
+        } else {
+            for (Dono dono : donos) {
+                System.out.println("Nome: " + dono.getNome());
+                System.out.println("Email: " + dono.getEmail());
+                System.out.println("Telefone: " + dono.getTelefone());
+                System.out.println("Endereço: " + dono.getEndereco());
+                System.out.println("-------------------------");
+            }
+        }
     }
 }
